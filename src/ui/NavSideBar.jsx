6d4ecf48078cb8ef336f99fefdad1319d22d3af2
Ridwan-Lawal <lawal6382@gmail.com>
2024-06-17 16:08:48 +0100

    built the nav / sidebar

--- src/ui/NavSideBar.jsx
+++ src/ui/NavSideBar.jsx
@@ -1,10 +1,10 @@
 function NavSideBar() {
   return (
-    <div className="h-[80px] md:w-[90px] md:rounded-r-2xl  overflow-hidden bg-ebony-clay md:h-screen">
+    <div className="h-[80px] md:w-[90px] md:rounded-r-[16px]  overflow-hidden bg-ebony-clay md:h-screen">
       {/* logo, theme switch, avatar */}
-      <div className="flex items-center justify-between">
+      <div className="flex  md:h-full md:flex-col items-center justify-between">
         {/* logo and theme switch */}
-        <div className="flex w-[90%] items-center justify-between border-r border-san-juan pr-6">
+        <div className="flex w-[90%]  md:h-[88%] md:w-full md:flex-col items-center justify-between border-r md:border-r-0 md:border-b border-san-juan pr-6 md:pr-0 md:pb-6">
           {/* logo */}
           <section className=" w-[80px]  h-[80px] md:h-[80px] md:w-full flex items-center justify-center bg-gradient-to-b from-san-juan to-cornflower-blue md:rounded-r-2xl">
             <img src="/logo.svg" alt="" />
@@ -17,7 +17,7 @@ function NavSideBar() {
         </div>
 
         {/* image avatar */}
-        <div className=" w-[11%] flex justify-center items-center">
+        <div className=" w-[11%] md:h-[15%] md:w-full  flex justify-center items-center">
           <img src="/image-avatar.jpg" alt="" className="w-10 rounded-full" />
         </div>
       </div>
